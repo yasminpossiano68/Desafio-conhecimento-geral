96874e49089cc0ff6f720f44f2250d7624ba4153
YASMIN FERREIRA POSSIANO <47806528806@info.local>
2026-03-17 21:38:00 -0300

    Alunos de Recuperacao

--- processamento.py
+++ processamento.py
@@ -1,2 +1,25 @@
+def validar_notas(notas):
+    if not notas or not isinstance(notas, list):
+        return False
+
+    for nota in notas:
+        if not isinstance(nota, (int, float)):
+            return False
+
+    return True
+
+
 def calcular_media(notas):
     return sum(notas) / len(notas)
+
+
+def alunos_recuperacao(alunos):
+    recuperacao = []
+
+    for nome, notas in alunos:
+        if validar_notas(notas):
+            media = calcular_media(notas)
+            if media < 7:
+                recuperacao.append((nome, media))
+
+    return recuperacao
